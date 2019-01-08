4247d1b1eca95a14a86981f43810fa469e376525
kan <kanyuds@gmail.com>
2019-01-08 09:37:18 +0700

    remake insert and reverse

--- skeleton-sp18/Disc_03/src/SLList.java
+++ skeleton-sp18/Disc_03/src/SLList.java
@@ -15,36 +15,69 @@ public class SLList {
         first = new IntNode(x, null);
         size = 1;
     }
+    public IntNode get(int i){
+        int start = 0;
+        IntNode temp = first;
+        while((temp != null) && (start < i)){
+            temp = temp.next;
+            start += 1;
+        }
+        return temp;
+    }
 
     public void addFirst(int x){
         first = new IntNode(x, first);
         size += 1;
     }
 
-    public void insert(int x, int p){
-        int i = 0;
-        IntNode temp = first;
-        while((temp != null) && (i < p - 1)){
-            temp = temp.next;
-            i += 1;
+    public void insert(int x, int i){
+        if(i == 0){
+            addFirst(x);
+        }else if(i == size-1){
+            IntNode temp = get(i);
+            temp.next = new IntNode(x, temp.next);
+            size += 1;
+        }else{
+            IntNode temp = get(i - 1);
+            temp.next = new IntNode(x, temp.next);
+            size += 1;
         }
-        temp.next = new IntNode(x, temp.next);
-        size += 1;
     }
 
+    public void print(){
+        IntNode printit = first;
+        while(printit != null){
+            System.out.printf("%s ",printit.item);
+            printit = printit.next;
+        }
+        System.out.println();
+    }
+
+//    public void reverse(){
+//        int i = 1;
+//        IntNode anchor;
+//        IntNode rest;
+//        while(i <= size){
+//            anchor = first;
+//            rest = first.next;
+//            while(rest.next != null){
+//                rest = rest.next;
+//            }
+//            rest.next = anchor;
+//            first = first.next;
+//            anchor.next = null;
+//            i += 1;
+//        }
+//    }
     public void reverse(){
-        int i = 1;
-        while(i < size){
-            IntNode anchor = first;
-            IntNode rest = first.next;
-            while(rest.next != null){
-                rest = rest.next;
-            }
-            rest.next = anchor;
-            first = first.next;
-            anchor.next = null;
+        int i = 0;
+        while(i <= size){
+            insert(get(size-1).item, i);
+            size -= 1;
             i += 1;
         }
+        IntNode temp = get(size-1);
+        temp.next = null;
     }
     private void reverseRecurHelper(int i){
 
@@ -59,15 +92,14 @@ public class SLList {
     }
 
     public static void main(String[] args){
-        SLList S = new SLList(2);
+        SLList S = new SLList(8);
         S.addFirst(7);
         S.addFirst(5);
         S.insert(6, 1);
-        S.addFirst(4);
-        S.addFirst(3);
-        S.addFirst(2);
-        S.addFirst(1);
-        S.addFirst(0);
+        S.insert(4, 0);
+        S.insert(9, 4);
+        S.print();
         S.reverse();
+        S.print();
     }
 }
